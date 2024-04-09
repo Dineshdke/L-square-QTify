a1418c68e8d98b5aaca2c4ae0d95659f052682cf
Dinesh Kumar E <egdedk@gmail.com>
2024-04-09 21:13:14 +0530

    Font change

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import HeroImage from "../../assets/Group 7483.png";
+import HeroImage from "../../assets/headphone.png";
 import styles from './Hero.module.css'
 
 
 export default function Hero() {
   return (
       <div className={styles.wrapper}>
+        <div className={styles.textHero}>
+            <div>
+                100 Thousand Songs, ad-free
+            </div>
+            <div>
+                Over thousands podcast episodes
+            </div>
+        </div>
         <img src={HeroImage} alt="HeroImage"/>
       </div>
   );
